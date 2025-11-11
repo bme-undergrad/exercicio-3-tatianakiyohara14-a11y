03638db85c22d105be84527f10fa469a2303c415
tatianakiyohara14-a11y <tatianakiyohara14@gmail.com>
2025-11-11 08:13:17 -0300

    Update exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -3,14 +3,18 @@ function [mu] = exercicio3(t, NC)
 % t: vetor de tempo com instâncias das medições
 % NC: medida do número de células a cada instante t
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+t_interv = t(1:3);
+NC_interv = NC(1:3);
 
-% digite seu codigo aqui
+%linearização
+b = log(NC_interv); 
 
+%ajuste de curva por regressão linear --> polyfit encontra coeficientes de um polinômio
+p = polyfit(t_interv, b, 1); 
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
+calculo_mu = p(1);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+mu = calculo_mu;
 
 endfunction
